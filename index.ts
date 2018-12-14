34b8cef849a6cde7046f899f60e0b986567149ea
Frank Luongo <frankluongo92@gmail.com>
2018-12-13 20:02:42 -0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+// Boolean
+const isOpen: boolean = false;
+
+// String
+const Name: string = 'Scott';
+
+// Number
+const myAge: number = 32;
+
+// List
+const list: number[] = [0, 1, 2];
+
+// Tuple
+const me: [string, number] = ["Scott", 32];
+
+// Enum
+enum Job { WebDev, WebDesigner, PM }
+const job: Job = Job.WebDev;
+
+// Any
+const phone: any = "iPhone";
+
+// Never
+// const tablet: never = 3;
+
+console.log(isOpen);
